96642aeddee294f418beef53931bded4850d83c5
urangawakgg <reservaindonesiagg@gmail.com>
2021-10-06 12:40:44 +0700

    after

--- dto/loginDto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type LoginDTO struct {
-	Username string `json:"username" form:"username" binding:"required"`
-	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:5"`
-}
